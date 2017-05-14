f3fbe87338c44c19267f2e0e9c4a45e7cf981767
taino <taino@surfandneptune.com>
2017-05-14 18:32:09 -0400

    Enable validation

--- Sources/Command.Description.swift
+++ Sources/Command.Description.swift
@@ -177,7 +177,7 @@ public struct Command {
 
 				if let subCommand = subCommands.first(where: { $0.name == next }) {
 					evaluation.subEvaluation =  try subCommand.subEvaluate(arguments: argsList)
-					//try evaluation.validate()
+					try evaluation.validate()
 					return evaluation
 
 				}
@@ -197,7 +197,7 @@ public struct Command {
 
 			}
 
-			//try evaluation.validate()
+			try evaluation.validate()
 
 			return evaluation
 		}
